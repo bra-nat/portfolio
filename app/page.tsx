95bcf75a621d302582fecfb5814c06458ec93055
bra-nat <126584626+bra-nat@users.noreply.github.com>
2023-12-27 15:18:23 +0000

    Setup Tailwind Custom Classes and Theme Properties

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 export default function Home() {
   return (
     <main className=" flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <h1 className="h1-bold">This is a test</h1>
+      <h2 className="h2-bold">This is a test</h2>
     </main>
   )
 }
